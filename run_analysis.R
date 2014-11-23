c5343112dcf6cdd5af30d8a7056a03b485ab8786
Ben Guidarelli <ben.guidarelli@twcable.com>
2014-11-23 14:50:23 -0500

    adding comments to all steps

--- run_analysis.R
+++ run_analysis.R
@@ -1,43 +1,40 @@
+/* Download & unzip the file */
+zippedFile <- "./uci.zip"
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", zippedFile, method="curl")
+unzip(zippedFile)
 
+/*Get the activity labels and feature labels*/
+activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt")
+feature_labels <- read.table("UCI HAR Dataset/features.txt")
 
-/*
-    Download the file
+/*Read in test and training data sets, joining the activity ids and subject ids on the left side */
+test_readings <- read.table("UCI HAR Dataset/test/X_test.txt", col.names=feature_labels$V2)
+test_readings[,"activity"] <- read.table("UCI HAR Dataset/test/y_test.txt")
+test_readings[,"subject"] <- read.table("UCI HAR Dataset/test/subject_test.txt")
 
-    zippedFile <- "./uci.zip"
-    download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", zippedFile, method="curl")
-    unzip(zippedFile)
+training_readings <- read.table("UCI HAR Dataset/test/X_test.txt", col.names=feature_labels$V2)
+training_readings[,"activity"] <- read.table("UCI HAR Dataset/test/y_test.txt")
+training_readings[,"subject"] <- read.table("UCI HAR Dataset/test/y_test.txt")
 
-    unzippedDir <- "UCI\ HAR\ Dataset"
+/* Create a combined list of both data sets */
+combined <- rbind(test_readings, training_readings)
 
-    activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt")
-    feature_labels <- read.table("UCI HAR Dataset/features.txt")
+/*Get the list of column names, pull out any coulmns that contain the words mean,Mean, or std */
+colNames <- names(combined)
+colsWeWant <- c(colNames[grep("mean|Mean|std", names(with_labels))], "activity", "subject")
+filtered <- combined[,colsWeWant]
 
-    test_readings <- read.table("UCI HAR Dataset/test/X_test.txt", col.names=feature_labels$V2)
-    test_readings[,"activity"] <- read.table("UCI HAR Dataset/test/y_test.txt")
-    test_readings[,"subject"] <- read.table("UCI HAR Dataset/test/subject_test.txt")
+/*Add in activity labels by joining on the activity labels table*/
+with_labels <- merge(filtered, activity_labels, by.x="activity", by.y="V1")
 
-    training_readings <- read.table("UCI HAR Dataset/test/X_test.txt", col.names=feature_labels$V2)
-    training_readings[,"activity"] <- read.table("UCI HAR Dataset/test/y_test.txt")
-    training_readings[,subject"] <- read.table("UCI HAR Dataset/test/y_test.txt")
+/*Create a melted data set with V2 being the activity label and subject being the subject ID*/
+melted <- melt(with_labels, id=c("V2", "subject"), measure.vars=(names(with_labels)))
 
-    combined <- rbind(test_readings, training_readings)
+/*Create a new table that generates an average for every subject and activity label against the features*/
+casted <- dcast(melted, subject+V2 ~ variable,mean)
 
-    colNames <- names(combined)
-    colsWeWant <- c(colNames[grep("mean|Mean|std", names(with_labels))], "activity", "subject")
-    filtered <- combined[,colsWeWant]
+/* Filter out a coulple of the extra columns */
+tidy <- casted[,!(names(casted) %in% c("activity.1", "V2"))]
 
-    with_labels <- merge(filtered, activity_labels, by.x="activity", by.y="V1")
-    
-    melted <- melt(with_labels, id=c("V2", "subject"), measure.vars=(names(with_labels)))
-
-    casted <- dcast(melted, subject+V2 ~ variable,mean)
-
-    tidy <- casted[,!(names(casted) %in% c("activity.1", "V2"))]
-
-    write.table(tidy, "tidy.txt", row.name=FALSE)
-
-*/
-
-
-
-
+/*Finally, write out the tidy data set to a file to be uploaded */
+write.table(tidy, "tidy.txt", row.name=FALSE)
